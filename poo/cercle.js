64812d75ee0b49fcee920ad6b774c6c8ee676f14
Yoann2N <yoanngervais.yg13750@gmail.com>
2025-02-03 16:52:45 +0100

    ok

--- poo/cercle.js
+++ poo/cercle.js
@@ -12,8 +12,8 @@ class Cercle {
     aire() {
         return Math.PI * Math.pow(this.rayon, 2);
     }
-}
+};
 
 let cercle1 = new Cercle(5);
-console.log(cercle1.perimetre)
-
+console.log(cercle1.perimetre())
+console.log(cercle1.aire())
