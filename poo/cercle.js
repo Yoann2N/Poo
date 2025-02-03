7d2b7502c4dc342f79a0dc76584d5d10926b565b
Yoann2N <yoanngervais.yg13750@gmail.com>
2025-02-03 16:45:34 +0100

    ok

---
+++ poo/cercle.js
@@ -0,0 +1,19 @@
+class Cercle {
+    constructor(rayon) {
+        this.rayon = rayon;
+    }
+
+    
+    perimetre() {
+        return 2 * Math.PI * this.rayon;
+    }
+
+    
+    aire() {
+        return Math.PI * Math.pow(this.rayon, 2);
+    }
+}
+
+let cercle1 = new Cercle(5);
+console.log(cercle1.perimetre)
+
